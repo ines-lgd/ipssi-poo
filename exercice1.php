a8863a5154f9d6d9e0f29043ad8b49984008d7d3
ines-lgd <i.legaud@ecole-ipssi.net>
2019-11-15 15:55:06 +0100

    Add files via upload

--- exercice1.php
+++ exercice1.php
@@ -9,15 +9,37 @@ class Diviseur {
     {
         $valeurs = [17, 12, 15, 38, 29, 157, 89, -22, 0, 5];
 
+
+        if ($diviseur === 0) {
+            //DivisionByZeroError
+            throw new Exception("Divison avec zéro impossible");
+        } /*elseif (!is_int($diviseur) || !is_int($index)) {
+            throw new Exception('Les saisies ne sont pas des nombres entiers');
+        } */elseif ($index > 10) {
+            throw new Exception('L\'indice saisi est supérieur au nombre de valeurs');
+        } elseif ($index < 0) {
+            throw new Exception('L\'indice saisi est négatif');
+        }
         return $valeurs[$index] / $diviseur;
     }
 }
 
-$input = $climate->input("Entrez l’indice de l’entier à diviser : ");
-$index = $input->prompt();
+do {
+    $input = $climate->input("Entrez l’indice de l’entier à diviser : ");
+    $index = $input->prompt();
 
-$input = $climate->input("Entrez le diviseur : ");
-$diviseur = $input->prompt();
+    $input = $climate->input("Entrez le diviseur : ");
+    $diviseur = $input->prompt();
+    $erreur = false;
 
-$climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+    try {
+        $climate->output("Le résultat de la division est : " . (new Diviseur())->division($index, $diviseur));
+        echo PHP_EOL;
+    } catch (Exception $e) {
+        $climate->output($e->getMessage());
+        echo PHP_EOL;
+        $erreur = true;
+    }
+} while ($erreur == true);
+
 
--- exercice2.php
+++ exercice2.php
@@ -2,6 +2,18 @@
 
 require_once('vendor/autoload.php');
 
-use Ipssi\Evaluation\Useless;
+$climate = new League\CLImate\CLImate;
+use Ipssi\Evaluation\Couleur;
+use Ipssi\Evaluation\Element;
+use Ipssi\Evaluation\Texte;
+use Ipssi\Evaluation\Image;
+use Ipssi\Evaluation\Forme;
+use Ipssi\Evaluation\Etoile;
+use Ipssi\Evaluation\Nuage;
 
-new Useless(); // Ceci ne sert à rien
+$couleur1 = new Couleur(15,18,19);
+$texte1 = new Texte(17,18, $couleur1, 'Bonjour à tous les amis', 'Comic Sans MS');
+$image1 = new Image(100, 50, 'PhpStorm.jpg');
+$nuage1 = new Nuage(200, 90, 'nuage',9);
+
+
